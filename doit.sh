96bf7940f0c280761b15e412c142ee5c55427360
Sergio Bruder <sergio@bruder.com.br>
2016-08-18 17:58:41 -0300

    scripts

---
+++ doit.sh
@@ -0,0 +1,1 @@
+ansible-playbook --inventory-file=hosts provision.yml
---
+++ encrypt-password.sh
@@ -0,0 +1,1 @@
+python -c "from passlib.hash import sha512_crypt; import getpass; print sha512_crypt.encrypt(getpass.getpass())"
